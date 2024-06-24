4fa1da2c2e63a68f4ec916f0ae948eb9071b976c
benjaminlutz <benlutz@pdx.edu>
2024-06-24 09:39:23 -0700

    Homescreen's looking pretty good

--- LanguageBud/ContentView.swift
+++ LanguageBud/ContentView.swift
@@ -43,34 +43,53 @@ struct ContentView: View {
             ZStack {
                 VStack {
                     Spacer()
+                        .frame(height: 4 * 75)
                     
                     if let image = selectedImage {
                         Image(uiImage: image)
                             .resizable()
                             .scaledToFit()
                             .frame(height: 200)
+                            .padding(0)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(color: .primary.opacity(0.5), radius: 4)
                     } else {
                         Text("No Image Selected")
                             .foregroundColor(.gray)
                             .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.white)
+                                    .shadow(color: .primary.opacity(0.5), radius: 4)
+                            )
+                            .padding()
                     }
 
                     if selectedImage != nil {
                         Button(action: {
                             isResultsViewPresented = true
                         }) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .padding()
-                                .frame(width: 200, height: 50)
-                                .background(.green)
+                            Image(systemName: "checkmark")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
                                 .foregroundColor(scheme == .dark ? Color.black : Color.white)
-                                .cornerRadius(10)
-                                .shadow(color: .primary.opacity(0.5), radius: 4)
+                                .padding()
+                                .background(
+                                    Circle()
+                                        .fill(Color.green)
+                                        .shadow(color: .primary.opacity(0.5), radius: 4)
+                                )
                         }
                         .navigationDestination(isPresented: $isResultsViewPresented) {
                             ResultsView(selectedImage: selectedImage!, language: languages[selectedLanguage] ?? "EN")
                         }
+                        .padding(30)
                     }
+
+
                     
                     Spacer()
                     
@@ -97,9 +116,9 @@ struct ContentView: View {
                         Button(action: {
                             isReviewMissedWordsViewPresented = true
                         }) {
-                            Text("Rev")
+                            Text("Review")
                                 .padding()
-                                .frame(width: 50, height: 50)
+                                .frame(width: 100, height: 50)
                                 .background(scheme == .dark ? Color.black : Color.white)
                                 .foregroundColor(.primary)
                                 .cornerRadius(10)
@@ -132,17 +151,26 @@ struct ContentView: View {
                     }
                 }
                 .frame(maxHeight: .infinity, alignment: .bottom)
+                .padding()
                 
-                DropdownView(
-                    title: "Select Language",
-                    prompt: "Select",
-                    options: Array(languages.keys.sorted())
-                ) { selected in
-                    selectedLanguage = selected
+                VStack {
+                    Text("Language Bud")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    
+                    DropdownView(
+                        title: "Select Language",
+                        prompt: "Select",
+                        options: Array(languages.keys.sorted())
+                    ) { selected in
+                        selectedLanguage = selected
+                    }
+                    .padding(.horizontal)
+                    .frame(maxHeight: .infinity, alignment: .top)
                 }
-                .padding(.horizontal)
-                .frame(maxHeight: .infinity, alignment: .top)
-                
             }
             .padding()
         }
